de995f7c9eb1bbd6010ef17006df3bc3106a2cb9
Alexander Zai <alexzai@amazon.com>
2018-05-30 15:14:47 -0700

    comment out init

--- src/x86/init.c
+++ src/x86/init.c
@@ -14,8 +14,8 @@ uint32_t cpuinfo_x86_clflush_size = 0;
 void cpuinfo_x86_init_processor(struct cpuinfo_x86_processor* processor) {
 	const struct cpuid_regs leaf0 = cpuid(0);
 	const uint32_t max_base_index = leaf0.eax;
-	const enum cpuinfo_vendor vendor = processor->vendor =
-		cpuinfo_x86_decode_vendor(leaf0.ebx, leaf0.ecx, leaf0.edx);
+//	const enum cpuinfo_vendor vendor = processor->vendor =
+//		cpuinfo_x86_decode_vendor(leaf0.ebx, leaf0.ecx, leaf0.edx);
 
 	const struct cpuid_regs leaf0x80000000 = cpuid(UINT32_C(0x80000000));
 	const uint32_t max_extended_index =
@@ -29,19 +29,19 @@ void cpuinfo_x86_init_processor(struct cpuinfo_x86_processor* processor) {
 		processor->cpuid = leaf1.eax;
 
 		const struct cpuinfo_x86_model_info model_info = cpuinfo_x86_decode_model_info(leaf1.eax);
-		const enum cpuinfo_uarch uarch = processor->uarch =
-			cpuinfo_x86_decode_uarch(vendor, &model_info);
+//		const enum cpuinfo_uarch uarch = processor->uarch =
+//			cpuinfo_x86_decode_uarch(vendor, &model_info);
 
 		cpuinfo_x86_clflush_size = ((leaf1.ebx >> 8) & UINT32_C(0x000000FF)) * 8;
 
 		cpuinfo_x86_detect_topology(max_base_index, max_extended_index, leaf1, &processor->topology);
 
-		#ifdef __native_client__
-			cpuinfo_isa = cpuinfo_x86_nacl_detect_isa();			
-		#else
-			cpuinfo_isa = cpuinfo_x86_detect_isa(leaf1, leaf0x80000001,
-				max_base_index, max_extended_index, vendor, uarch);
-		#endif
+//		#ifdef __native_client__
+//			cpuinfo_isa = cpuinfo_x86_nacl_detect_isa();
+//		#else
+//			cpuinfo_isa = cpuinfo_x86_detect_isa(leaf1, leaf0x80000001,
+//				max_base_index, max_extended_index, vendor, uarch);
+//		#endif
 	}
 	if (max_extended_index >= UINT32_C(0x80000004)) {
 		struct cpuid_regs brand_string[3];
