5c6c77e39d1b9391f07eed523414203da2a2288d
Alexander Zai <alexzai@amazon.com>
2018-05-31 12:00:46 -0700

    remove restrict

--- src/linux/cpuinfo.cc
+++ src/linux/cpuinfo.cc
@@ -49,7 +49,7 @@ static uint32_t parse_processor_number(
 static void parse_apic_id(
 	const char* apic_start,
 	const char* apic_end,
-	struct cpuinfo_x86_linux_processor processor[static 1])
+	struct cpuinfo_x86_linux_processor processor[1])
 {
 	uint32_t apic_id = 0;
 	for (const char* digit_ptr = apic_start; digit_ptr != apic_end; digit_ptr++) {
@@ -82,7 +82,7 @@ struct proc_cpuinfo_parser_state {
 static bool parse_line(
 	const char* line_start,
 	const char* line_end,
-	struct proc_cpuinfo_parser_state state[static 1],
+	struct proc_cpuinfo_parser_state state[1],
 	uint64_t line_number)
 {
 	/* Empty line. Skip. */
