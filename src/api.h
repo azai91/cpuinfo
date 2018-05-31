e8710bd4a40a3dd0546002663119d144205c0dda
Alexander Zai <alexzai@amazon.com>
2018-05-31 12:16:22 -0700

    comment out code

--- src/api.h
+++ src/api.h
@@ -86,11 +86,11 @@ struct cpuinfo_x86_processor {
 
 void cpuinfo_x86_init_processor(struct cpuinfo_x86_processor* processor);
 
-enum cpuinfo_vendor cpuinfo_x86_decode_vendor(uint32_t ebx, uint32_t ecx, uint32_t edx);
-struct cpuinfo_x86_model_info cpuinfo_x86_decode_model_info(uint32_t eax);
-enum cpuinfo_uarch cpuinfo_x86_decode_uarch(
-	enum cpuinfo_vendor vendor,
-	const struct cpuinfo_x86_model_info* model_info);
+//enum cpuinfo_vendor cpuinfo_x86_decode_vendor(uint32_t ebx, uint32_t ecx, uint32_t edx);
+//struct cpuinfo_x86_model_info cpuinfo_x86_decode_model_info(uint32_t eax);
+//enum cpuinfo_uarch cpuinfo_x86_decode_uarch(
+//	enum cpuinfo_vendor vendor,
+//	const struct cpuinfo_x86_model_info* model_info);
 
 struct cpuinfo_x86_isa cpuinfo_x86_detect_isa(
 	const struct cpuid_regs basic_info, const struct cpuid_regs extended_info,
@@ -104,26 +104,26 @@ void cpuinfo_x86_detect_topology(
 	struct cpuid_regs leaf1,
 	struct cpuinfo_x86_topology* topology);
 
-void cpuinfo_x86_detect_cache(
-	uint32_t max_base_index, uint32_t max_extended_index,
-	bool amd_topology_extensions,
-	enum cpuinfo_vendor vendor,
-	const struct cpuinfo_x86_model_info* model_info,
-	struct cpuinfo_x86_caches* cache,
-	struct cpuinfo_tlb* itlb_4KB,
-	struct cpuinfo_tlb* itlb_2MB,
-	struct cpuinfo_tlb* itlb_4MB,
-	struct cpuinfo_tlb* dtlb0_4KB,
-	struct cpuinfo_tlb* dtlb0_2MB,
-	struct cpuinfo_tlb* dtlb0_4MB,
-	struct cpuinfo_tlb* dtlb_4KB,
-	struct cpuinfo_tlb* dtlb_2MB,
-	struct cpuinfo_tlb* dtlb_4MB,
-	struct cpuinfo_tlb* dtlb_1GB,
-	struct cpuinfo_tlb* stlb2_4KB,
-	struct cpuinfo_tlb* stlb2_2MB,
-	struct cpuinfo_tlb* stlb2_1GB,
-	uint32_t* log2_package_cores_max);
+//void cpuinfo_x86_detect_cache(
+//	uint32_t max_base_index, uint32_t max_extended_index,
+//	bool amd_topology_extensions,
+//	enum cpuinfo_vendor vendor,
+//	const struct cpuinfo_x86_model_info* model_info,
+//	struct cpuinfo_x86_caches* cache,
+//	struct cpuinfo_tlb* itlb_4KB,
+//	struct cpuinfo_tlb* itlb_2MB,
+//	struct cpuinfo_tlb* itlb_4MB,
+//	struct cpuinfo_tlb* dtlb0_4KB,
+//	struct cpuinfo_tlb* dtlb0_2MB,
+//	struct cpuinfo_tlb* dtlb0_4MB,
+//	struct cpuinfo_tlb* dtlb_4KB,
+//	struct cpuinfo_tlb* dtlb_2MB,
+//	struct cpuinfo_tlb* dtlb_4MB,
+//	struct cpuinfo_tlb* dtlb_1GB,
+//	struct cpuinfo_tlb* stlb2_4KB,
+//	struct cpuinfo_tlb* stlb2_2MB,
+//	struct cpuinfo_tlb* stlb2_1GB,
+//	uint32_t* log2_package_cores_max);
 
 void cpuinfo_x86_decode_cache_descriptor(
 	uint8_t descriptor, enum cpuinfo_vendor vendor,
